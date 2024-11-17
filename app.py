207ed28ef425c23d3252772e4bd6fa84e1261704
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-11-17 15:02:01 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+from flask_cors import CORS
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Initialize Flask-SocketIO
 socketio = SocketIO(app)
