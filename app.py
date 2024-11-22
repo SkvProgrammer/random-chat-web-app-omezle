456e5c65944b5bc620571a131a0acf68ca27f829
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-11-22 11:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import os
-import socket
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['PREFERRED_URL_SCHEME'] = 'https'
 
-CORS(app, resources={r"/*": {"origins": ["https://yourdomain.com", "https://sub.yourdomain.com"]}})
+#CORS(app, resources={r"/*": {"origins": ["https://yourdomain.com", "https://sub.yourdomain.com"]}})
 
 # Initialize Flask-SocketIO
 socketio = SocketIO(app)
@@ -78,9 +78,4 @@ def handle_error(e):
     print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
-    # Bind to any available port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('', 0))  # Let the OS assign an available port
-        port = s.getsockname()[1]  # Retrieve the assigned port
-    print(f"Running on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app)
